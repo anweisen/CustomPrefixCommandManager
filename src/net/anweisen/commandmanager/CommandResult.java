2d7e7299159a97c83ba11cacdffe2de06669fc96
anweisen <60273047+anweisen@users.noreply.github.com>
2020-05-28 14:28:15 +0200

    Advanced CommandResult-System

--- src/net/anweisen/commandmanager/CommandResult.java
+++ src/net/anweisen/commandmanager/CommandResult.java
@@ -6,11 +6,49 @@ package net.anweisen.commandmanager;
  * Website: www.anweisen.net
  */
 
-public enum CommandResult {
+public class CommandResult {
 
-    INVALID_CHANNEL,
-    PREFIX_NOT_USED,
-    COMMAND_NOT_FOUND,
-    SUCCESS;
+    public static final CommandResult NORMAL_RESULT = new CommandResult(CommandResult.ResultType.SUCCESS);
+
+    public enum ResultType {
+
+        INVALID_CHANNEL,
+        PREFIX_NOT_USED,
+        COMMAND_NOT_FOUND,
+        SUCCESS,
+        UNKNOWN;
+
+    }
+
+    private String message;
+    private ResultType type;
+
+    public CommandResult(ResultType type, String message) {
+        this.type = type;
+        this.message = message;
+    }
+
+    public CommandResult(ResultType type) {
+        this.type = type;
+        this.message = type.name();
+    }
+
+    public CommandResult(String message) {
+        this.type = ResultType.UNKNOWN;
+        this.message = message;
+    }
+
+    public CommandResult() {
+        this.type = ResultType.UNKNOWN;
+        this.message = "Unknown";
+    }
+
+    public ResultType getType() {
+        return type;
+    }
+
+    public String getMessage() {
+        return message;
+    }
 
 }
