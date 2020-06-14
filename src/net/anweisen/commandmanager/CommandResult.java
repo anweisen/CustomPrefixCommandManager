322a4f8d6edab9d5b3e87717f2ce86fb9c03ee56
anweisen <60273047+anweisen@users.noreply.github.com>
2020-06-14 19:49:05 +0200

    Custom CommandResults removed

--- src/net/anweisen/commandmanager/CommandResult.java
+++ src/net/anweisen/commandmanager/CommandResult.java
@@ -6,51 +6,14 @@ package net.anweisen.commandmanager;
  * Website: www.anweisen.net
  */
 
-public class CommandResult {
+public enum CommandResult {
 
-    public static final CommandResult NORMAL_RESULT = new CommandResult(ResultType.SUCCESS);
-
-    public enum ResultType {
-
-        INVALID_CHANNEL,
-        WEBHOOK_MESSAGE_NO_REACT,
-        PREFIX_NOT_USED,
-        COMMAND_NOT_FOUND,
-        SUCCESS,
-        USER_SPECIFIED,
-        UNKNOWN;
-
-    }
-
-    private String message;
-    private ResultType type;
-
-    public CommandResult(ResultType type, String message) {
-        this.type = type;
-        this.message = message;
-    }
-
-    public CommandResult(ResultType type) {
-        this.type = type;
-        this.message = "null";
-    }
-
-    public CommandResult(String message) {
-        this.type = ResultType.USER_SPECIFIED;
-        this.message = message;
-    }
-
-    public CommandResult() {
-        this.type = ResultType.UNKNOWN;
-        this.message = "null";
-    }
-
-    public ResultType getType() {
-        return type;
-    }
-
-    public String getMessage() {
-        return message;
-    }
+    INVALID_CHANNEL_PRIVATE_COMMAND,
+    INVALID_CHANNEL_GUILD_COMMAND,
+    WEBHOOK_MESSAGE_NO_REACT,
+    PREFIX_NOT_USED,
+    COMMAND_NOT_FOUND,
+    SUCCESS,
+    UNKNOWN;
 
 }
