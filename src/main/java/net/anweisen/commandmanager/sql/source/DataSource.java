641e3eaee6f49777fe0a0b1e35f5187a7b386881
anweisen <crafter8.0@web.de>
2020-09-09 21:06:46 +0200

    Comments for methods added

--- src/main/java/net/anweisen/commandmanager/sql/source/DataSource.java
+++ src/main/java/net/anweisen/commandmanager/sql/source/DataSource.java
@@ -1,5 +1,6 @@
 package net.anweisen.commandmanager.sql.source;
 
+import javax.annotation.CheckReturnValue;
 import javax.annotation.Nonnull;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -17,12 +18,23 @@ public interface DataSource {
 	String MYSQL_URL = "jdbc:mysql://%host/%database",
 		   LITESQL_URL = "jdbc:sqlite:%file";
 
+	/**
+	 * @return Creates a connection to the SQL server using the url ({@link DataSource#getURL()}) (Default: using {@link DriverManager#getConnection(String)})
+	 * @throws SQLException If a {@link SQLException} happens while creating the connection
+	 * @see DriverManager#getConnection(String)
+	 */
 	@Nonnull
+	@CheckReturnValue
 	default Connection createConnection() throws SQLException {
 		return DriverManager.getConnection(getURL());
 	}
 
+	/**
+	 * @return The URL which is used to connect to a database
+	 * @see DataSource#createConnection()
+	 */
 	@Nonnull
+	@CheckReturnValue
 	String getURL();
 
 }
