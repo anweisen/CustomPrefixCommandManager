22dc9e3371b52df6a69fcb3dd385ced22af62c5c
anweisen <crafter8.0@web.de>
2020-09-10 21:08:09 +0200

    Added resultHandler

--- src/main/java/net/anweisen/commandmanager/defaults/DefaultMessageListener.java
+++ src/main/java/net/anweisen/commandmanager/defaults/DefaultMessageListener.java
@@ -1,6 +1,7 @@
 package net.anweisen.commandmanager.defaults;
 
 import net.anweisen.commandmanager.CommandHandler;
+import net.anweisen.commandmanager.CommandResult;
 import net.anweisen.commandmanager.listener.EventHandler;
 import net.anweisen.commandmanager.listener.Listener;
 import net.anweisen.commandmanager.utils.Factory;
@@ -8,22 +9,22 @@ import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 
 import javax.annotation.Nonnull;
+import java.util.function.BiConsumer;
 
 /**
- * Developed in the CommandManager project
- * on 08-23-2020
- *
  * @author anweisen | https://github.com/anweisen
  * @since 1.2
  */
 public final class DefaultMessageListener implements Listener {
 
 	private final CommandHandler commandHandler;
-	private final Factory<String, Guild> prefix;
+	private Factory<String, Guild> prefix;
+	private BiConsumer<MessageReceivedEvent, CommandResult> resultHandler;
 
 	public DefaultMessageListener(@Nonnull CommandHandler commandHandler, @Nonnull String prefix) {
 		this.commandHandler = commandHandler;
 		this.prefix = guild -> prefix;
+		this.resultHandler = new DefaultResultHandler();
 	}
 
 	/**
@@ -32,11 +33,29 @@ public final class DefaultMessageListener implements Listener {
 	public DefaultMessageListener(@Nonnull CommandHandler commandHandler, @Nonnull Factory<String, Guild> prefix) {
 		this.commandHandler = commandHandler;
 		this.prefix = prefix;
+		this.resultHandler = new DefaultResultHandler();
+	}
+
+	public DefaultMessageListener(@Nonnull CommandHandler commandHandler, @Nonnull Factory<String, Guild> prefix, @Nonnull BiConsumer<MessageReceivedEvent, CommandResult> resultHandler) {
+		this.commandHandler = commandHandler;
+		this.prefix = prefix;
+		this.resultHandler = resultHandler;
+	}
+
+	public void setPrefix(@Nonnull Factory<String, Guild> prefix) {
+		this.prefix = prefix;
+	}
+
+	public void setResultHandler(BiConsumer<MessageReceivedEvent, CommandResult> resultHandler) {
+		this.resultHandler = resultHandler;
 	}
 
 	@EventHandler
 	public void onMessage(MessageReceivedEvent event) {
-		commandHandler.handleCommand(prefix.get(event.isFromGuild() ? event.getGuild() : null), event);
+		CommandResult result = commandHandler.handleCommand(prefix.get(event.isFromGuild() ? event.getGuild() : null), event);
+		if (resultHandler != null) {
+			resultHandler.accept(event, result);
+		}
 	}
 
 }
