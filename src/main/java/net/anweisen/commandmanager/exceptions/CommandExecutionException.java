6a21ad1c9d8aa5531d64efdf828e45035df5fee5
anweisen <crafter8.0@web.de>
2020-09-11 22:36:42 +0200

    Added CommandExcecutionException

---
+++ src/main/java/net/anweisen/commandmanager/exceptions/CommandExecutionException.java
@@ -0,0 +1,24 @@
+package net.anweisen.commandmanager.exceptions;
+
+/**
+ * @author anweisen | https://github.com/anweisen
+ * @since 2.1
+ */
+public class CommandExecutionException extends RuntimeException {
+
+	public CommandExecutionException() {
+	}
+
+	public CommandExecutionException(String message) {
+		super(message);
+	}
+
+	public CommandExecutionException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public CommandExecutionException(Throwable cause) {
+		super(cause);
+	}
+
+}
