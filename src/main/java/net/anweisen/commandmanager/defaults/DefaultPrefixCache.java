a8928590998124b86cd809c3e6c035491b332c7d
anweisen <crafter8.0@web.de>
2020-09-12 21:12:09 +0200

    Extending SQLValueCache

--- src/main/java/net/anweisen/commandmanager/defaults/DefaultPrefixCache.java
+++ src/main/java/net/anweisen/commandmanager/defaults/DefaultPrefixCache.java
@@ -1,232 +1,41 @@
 package net.anweisen.commandmanager.defaults;
 
 import net.anweisen.commandmanager.sql.SQL;
+import net.anweisen.commandmanager.sql.cache.SQLValueCache;
 import net.anweisen.commandmanager.utils.Bindable;
 import net.anweisen.commandmanager.utils.Factory;
 import net.dv8tion.jda.api.entities.Guild;
+import org.jetbrains.annotations.NotNull;
 
 import javax.annotation.CheckReturnValue;
 import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Timer;
-import java.util.TimerTask;
 
 /**
  * @author anweisen | https://github.com/anweisen
  * @since 2.1
  */
-public final class DefaultPrefixCache implements Factory<String, Guild>, Bindable {
+public final class DefaultPrefixCache extends SQLValueCache implements Factory<String, Guild>, Bindable {
 
-	//                          guildID, prefix
-	private final transient Map<String, String> cache = new HashMap<>();
-	private boolean cachePrefix;
-
-	private String defaultPrefix;
-
-	private final String table, guildColumn, prefixColumn;
-	private final SQL dataSource;
-
-	private int clearRate = 3*60;
-	private Timer timer;
-
-	public DefaultPrefixCache(@Nonnull String defaultPrefix, @Nonnull SQL dataSource) {
-		this(true, defaultPrefix, "server", "guildID", "prefix", dataSource);
+	public DefaultPrefixCache(@Nonnull SQL data, @Nonnull String defaultPrefix) {
+		super(data, "server", "guildID", "prefix", defaultPrefix);
 	}
 
-	public DefaultPrefixCache(boolean cachePrefix, @Nonnull String defaultPrefix, @Nonnull String table, @Nonnull String guildColumn, @Nonnull String prefixColumn, @Nonnull SQL dataSource) {
-		this.cachePrefix = cachePrefix;
-		this.defaultPrefix = defaultPrefix;
-		this.table = table;
-		this.guildColumn = guildColumn;
-		this.prefixColumn = prefixColumn;
-		this.dataSource = dataSource;
-		checkDatabaseTable();
-		setupTimer();
+	public DefaultPrefixCache(@NotNull SQL data, @Nonnull String table, @Nonnull String keyColumn, @Nonnull String valueColumn, @Nonnull String defaultValue) {
+		super(data, table, keyColumn, valueColumn, defaultValue);
 	}
 
-	private void checkDatabaseTable() {
-		try {
-			dataSource.update("CREATE TABLE IF NOT EXISTS " + table + " (" + guildColumn + " VARCHAR(18), " + prefixColumn + " VARCHAR(1000))");
-		} catch (SQLException ex) {
-			throw new ExceptionInInitializerError(ex);
-		}
-	}
-
-	private void setupTimer() {
-		if (timer != null) timer.cancel();
-		if (clearRate < 0) return;
-		timer = new Timer();
-		timer.schedule(new TimerTask() {
-			@Override
-			public void run() {
-				cache.clear();
-			}
-		}, 0, clearRate * 1000);
+	public DefaultPrefixCache(boolean cacheValues, @Nonnull String defaultValue, @Nonnull SQL data, @Nonnull String table, @Nonnull String keyColumn, @Nonnull String valueColumn, int clearRate) {
+		super(cacheValues, defaultValue, data, table, keyColumn, valueColumn, clearRate);
 	}
 
 	/**
-	 * @param guildID The key, the prefix is stored to
-	 * @return The prefix which is currently saved in this class. It will be null if no prefix is cached
-	 */
-	@Nullable
-	@CheckReturnValue
-	public String getCached(@Nonnull String guildID) {
-		return cache.get(guildID);
-	}
-
-	@CheckReturnValue
-	public boolean isCached(@Nonnull String guildID) {
-		return getCached(guildID) != null;
-	}
-
-	/**
-	 * @param guildID The guild, the prefix is assigned to
-	 * @param prefix The prefix which should be stored. If null, the entry will be removed
-	 * @exception IllegalStateException If not {@link DefaultPrefixCache#shouldCachePrefix()}
-	 */
-	public synchronized void setCached(@Nonnull String guildID, String prefix) {
-		if (!cachePrefix) throw new IllegalStateException();
-		if (prefix == null) {
-			cache.remove(guildID);
-		} else {
-			cache.put(guildID, prefix);
-		}
-	}
-
-	/**
-	 * @param guildID The key, the prefix is saved to
-	 * @return The prefix saved in the database by the given guildID
-	 * @throws SQLException If a {@link SQLException} happens while using sql
-	 * @exception IllegalStateException If the database does not contain the given guildID
-	 */
-	public String getFromDatabase(String guildID) throws SQLException {
-		ResultSet result = dataSource.query("SELECT " + prefixColumn + " FROM " + table + " WHERE " + guildColumn + " = ?", guildID);
-		if (!result.next()) throw new IllegalStateException("Database does not contain the given guild");
-		String prefix = result.getString(prefixColumn);
-		result.close();
-		return prefix;
-	}
-
-	/**
-	 * Reads the prefix from the database by the given. If not prefix was found it will return the <code>defaultPrefix</code>.
-	 * If {@link DefaultPrefixCache#shouldCachePrefix()}, it'll cache the prefix as well (using {@link DefaultPrefixCache#setCached(String, String)})
-	 * @return The prefix loaded
-	 */
-	@Nonnull
-	public String load(@Nonnull String guildID) {
-
-		String prefix = defaultPrefix;
-
-		try {
-			prefix = getFromDatabase(guildID);
-		} catch (SQLException ex) {
-			ex.printStackTrace();
-		} catch (IllegalStateException ignored) { }
-
-		if (prefix == null) prefix = defaultPrefix;
-
-		if (cachePrefix) {
-			setCached(guildID, prefix);
-		}
-
-		return prefix;
-
-	}
-
-	/**
-	 * @return The cached prefix, if {@link DefaultPrefixCache#shouldCachePrefix()} is enabled.
-	 *         Otherwise it will load the prefix using {@link DefaultPrefixCache#load(String)}
-	 */
-	@Nonnull
-	@CheckReturnValue
-	public String getPrefix(@Nonnull String guildID) {
-
-		if (cachePrefix) {
-			String prefix = getCached(guildID);
-			if (prefix != null) {
-				return prefix;
-			}
-		}
-
-		return load(guildID);
-
-	}
-
-	/**
-	 * Sets a prefix for the given guildID and writes it into the database.
-	 * If {@link DefaultPrefixCache#shouldCachePrefix()}, it will also cache the prefix (using {@link DefaultPrefixCache#setCached(String, String)})
-	 * @param guildID The guildID, the prefix should be saved to
-	 * @param prefix The prefix to set
-	 * @throws SQLException If a {@link SQLException} is thrown while executing the sql request
-	 */
-	public synchronized void set(String guildID, String prefix) throws SQLException {
-		if (cachePrefix) setCached(guildID, prefix);
-		try {
-			getFromDatabase(guildID); // Checks if the guild is in the database (throws IllegalStateException if not)
-			dataSource.update("UPDATE " + table + " SET " + prefixColumn + " = ? WHERE " + guildColumn + " = ?", prefix, guildID);
-		} catch (IllegalStateException ignored) {
-			dataSource.update("INSERT INTO " + table + " (" + guildColumn + ", " + prefixColumn + ") VALUES (?, ?)", guildID, prefix);
-		}
-	}
-
-	/**
-	 * Loads the prefix using {@link DefaultPrefixCache#load(String)}, by the given guild ({@link Guild#getId()})
-	 * If the guild is null, it will return the <code>defaultPrefix</code>
-	 * @param guild The guild, whose id ({@link Guild#getId()}) will be used to load the prefix. If null, it will return the <code>defaultPrefix</code>
-	 * @return Returns the prefix loaded
+	 * Uses {@link Guild#getId()} as the key and passes it to {@link SQLValueCache#get(String)}
 	 */
 	@Nonnull
 	@Override
+	@CheckReturnValue
 	public String get(Guild guild) {
-		if (guild == null) {
-			return defaultPrefix;
-		} else {
-			return getPrefix(guild.getId());
-		}
-	}
-
-	public void setDefaultPrefix(@Nonnull String defaultPrefix) {
-		this.defaultPrefix = defaultPrefix;
-	}
-
-	@Nonnull
-	@CheckReturnValue
-	public String getDefaultPrefix() {
-		return defaultPrefix;
-	}
-
-	@Nonnull
-	@CheckReturnValue
-	public SQL getDataSource() {
-		return dataSource;
-	}
-
-	@CheckReturnValue
-	public boolean shouldCachePrefix() {
-		return cachePrefix;
-	}
-
-	/**
-	 * Sets if, when a prefix was loaded, it will be stored and
-	 */
-	public void setCachePrefix(boolean cache) {
-		if (this.cachePrefix != cache) {
-			this.cache.clear();
-		}
-		this.cachePrefix = cache;
-	}
-
-	public int getClearRate() {
-		return clearRate;
-	}
-
-	public void setClearRate(int clearRate) {
-		this.clearRate = clearRate;
-		setupTimer();
+		return super.get(guild == null ? null : guild.getId());
 	}
 
 }
