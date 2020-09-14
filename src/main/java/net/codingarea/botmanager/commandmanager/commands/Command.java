a50cec6b97855f93a00e6173fa7912a014d4ec19
anweisen <crafter8.0@web.de>
2020-09-14 19:47:11 +0200

    Added throws declaration to onCommand

---
+++ src/main/java/net/codingarea/botmanager/commandmanager/commands/Command.java
@@ -0,0 +1,113 @@
+package net.codingarea.botmanager.commandmanager.commands;
+
+import net.codingarea.botmanager.commandmanager.CommandEvent;
+import net.codingarea.botmanager.commandmanager.CommandType;
+import net.dv8tion.jda.api.Permission;
+import org.jetbrains.annotations.Nullable;
+
+import javax.annotation.Nonnull;
+
+/**
+ * @author anweisen | https://github.com/anweisen
+ * @since 1.2
+ */
+public abstract class Command implements ICommand {
+
+	public Command(@Nonnull String name, @Nonnull String... alias) {
+		this(name, false, alias);
+	}
+
+	public Command(@Nonnull String name, boolean processInNewThread, @Nonnull String... alias) {
+		this.name = name;
+		this.alias = alias;
+		this.processInNewThread = processInNewThread;
+	}
+
+	public Command(@Nonnull String name, @Nonnull CommandType commandType, @Nonnull String... alias) {
+		this(name, alias);
+		this.type = commandType;
+	}
+
+	public Command(@Nonnull String name, @Nonnull Permission permission, @Nonnull String... alias) {
+		this(name, alias);
+		this.permission = permission;
+	}
+
+	public Command(@Nonnull String name, @Nonnull CommandType commandType, boolean processInNewThread, @Nonnull String... alias) {
+		this(name, processInNewThread, alias);
+		this.type = commandType;
+	}
+
+	public Command(@Nonnull String name, boolean processInNewThread, boolean reactToMentionPrefix, @Nonnull String... alias) {
+		this(name, processInNewThread, alias);
+		this.reactToMentionPrefix = reactToMentionPrefix;
+	}
+
+	public Command(@Nonnull String name, @Nonnull CommandType commandType, boolean processInNewThread, boolean reactToMentionPrefix, @Nonnull String... alias) {
+		this(name, commandType, processInNewThread, alias);
+		this.reactToMentionPrefix = reactToMentionPrefix;
+	}
+
+	public Command(@Nonnull String name, boolean processInNewThread, boolean reactToMentionPrefix, @Nonnull Permission permission, @Nonnull String... alias) {
+		this(name, CommandType.GUILD, processInNewThread, reactToMentionPrefix, alias);
+		this.permission = permission;
+	}
+
+	private final String name;
+	private final String[] alias;
+
+	private final boolean processInNewThread;
+	private Permission permission;
+
+	private CommandType type = CommandType.GENERAL;
+	private boolean reactToWebhooks = false;
+	private boolean reactToBots = false;
+	private boolean reactToMentionPrefix = true;
+
+	public abstract void onCommand(@Nonnull final CommandEvent event) throws Throwable;
+
+	@Nonnull
+	@Override
+	public final CommandType getType() {
+		return type;
+	}
+
+	@Nonnull
+	@Override
+	public final String[] getAlias() {
+		return alias;
+	}
+
+	@Nonnull
+	@Override
+	public final String getName() {
+		return name;
+	}
+
+	@Nullable
+	@Override
+	public Permission getPermissionNeeded() {
+		return permission;
+	}
+
+	@Override
+	public final boolean shouldReactToWebhooks() {
+		return reactToWebhooks;
+	}
+
+	@Override
+	public final boolean shouldReactToBots() {
+		return reactToBots;
+	}
+
+	@Override
+	public final boolean shouldProcessInNewThread() {
+		return processInNewThread;
+	}
+
+	@Override
+	public final boolean shouldReactToMentionPrefix() {
+		return reactToMentionPrefix;
+	}
+
+}
---
+++ src/main/java/net/codingarea/botmanager/commandmanager/commands/ICommand.java
@@ -0,0 +1,48 @@
+package net.codingarea.botmanager.commandmanager.commands;
+
+import net.codingarea.botmanager.commandmanager.CommandEvent;
+import net.codingarea.botmanager.commandmanager.CommandType;
+import net.dv8tion.jda.api.Permission;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+/**
+ * @author anweisen | https://github.com/anweisen
+ * @since 1.2
+ */
+public interface ICommand {
+
+	void onCommand(@Nonnull final CommandEvent event) throws Throwable;
+
+	@Nonnull
+	String getName();
+
+	@Nonnull
+	String[] getAlias();
+
+	@Nullable
+	Permission getPermissionNeeded();
+
+	@Nonnull
+	default CommandType getType() {
+		return CommandType.GENERAL;
+	}
+
+	default boolean shouldReactToWebhooks() {
+		return false;
+	}
+
+	default boolean shouldReactToBots() {
+		return false;
+	}
+
+	default boolean shouldProcessInNewThread() {
+		return false;
+	}
+
+	default boolean shouldReactToMentionPrefix() {
+		return false;
+	}
+
+}
