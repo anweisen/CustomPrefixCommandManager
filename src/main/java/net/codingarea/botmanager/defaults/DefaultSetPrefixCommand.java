48f654d065062a8dbaa996723432e1ac24f47af1
anweisen <crafter8.0@web.de>
2020-09-17 22:44:38 +0200

    Added DefaultSetPrefixCommand

---
+++ src/main/java/net/codingarea/botmanager/defaults/DefaultSetPrefixCommand.java
@@ -0,0 +1,41 @@
+package net.codingarea.botmanager.defaults;
+
+import net.codingarea.botmanager.commandmanager.CommandEvent;
+import net.codingarea.botmanager.commandmanager.commands.Command;
+import net.dv8tion.jda.api.Permission;
+import org.jetbrains.annotations.NotNull;
+
+import javax.annotation.Nonnull;
+
+/**
+ * @author anweisen | https://github.com/anweisen
+ * @since 2.3
+ */
+public final class DefaultSetPrefixCommand extends Command {
+
+	private final DefaultPrefixCache cache;
+
+	public DefaultSetPrefixCommand(@Nonnull String name, @Nonnull Permission permission, @Nonnull DefaultPrefixCache cache, @Nonnull String... alias) {
+		super(name, permission, alias);
+		this.cache = cache;
+	}
+
+	@Override
+	public void onCommand(@Nonnull final CommandEvent event) throws Throwable {
+
+		String prefix = event.getArgsAsString().replace("`", "");
+		if (prefix.isEmpty()) {
+			event.queueReply("Please use " + event.syntax("<prefix>"));
+			return;
+		}
+		if (prefix.length() > 10) {
+			event.queueReply("Your prefix cannot be longer than 10 characters.");
+			return;
+		}
+
+		cache.set(event.getGuildID(), prefix);
+		event.queueReply("Your prefix was set to `" + prefix + "`");
+
+	}
+
+}
