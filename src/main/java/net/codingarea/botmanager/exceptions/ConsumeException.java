47e1f6f9da2f23754a8b8b48a665a80f40dd325c
anweisen <crafter8.0@web.de>
2020-09-22 17:18:46 +0200

    Renamed ConsumerExecutionException

---
+++ src/main/java/net/codingarea/botmanager/exceptions/ConsumeException.java
@@ -0,0 +1,24 @@
+package net.codingarea.botmanager.exceptions;
+
+/**
+ * @author anweisen | https://github.com/anweisen
+ * @since 2.3
+ */
+public class ConsumeException extends ExecutionException {
+
+	public ConsumeException() {
+		super();
+	}
+
+	public ConsumeException(String message) {
+		super(message);
+	}
+
+	public ConsumeException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public ConsumeException(Throwable cause) {
+		super(cause);
+	}
+}
