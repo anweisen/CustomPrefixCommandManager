2a63b638ddd8bac47dcf3a0ffe8a8a7c553c5b5c
anweisen <crafter8.0@web.de>
2020-09-22 17:20:12 +0200

    Added @CheckReturnValue to constructors

--- src/main/java/net/codingarea/botmanager/defaults/DefaultPrefixCache.java
+++ src/main/java/net/codingarea/botmanager/defaults/DefaultPrefixCache.java
@@ -14,19 +14,23 @@ import javax.annotation.Nonnull;
  */
 public final class DefaultPrefixCache extends SQLValueCache implements Factory<String, Guild> {
 
+	@CheckReturnValue
 	public DefaultPrefixCache(@Nonnull SQL data, @Nonnull String defaultPrefix) {
 		super(data, "server", "guildID", "prefix", defaultPrefix);
 	}
 
+	@CheckReturnValue
 	public DefaultPrefixCache(@Nonnull SQL data, @Nonnull String table, @Nonnull String keyColumn, @Nonnull String valueColumn, @Nonnull String defaultPrefix) {
 		super(data, table, keyColumn, valueColumn, defaultPrefix);
 	}
 
+	@CheckReturnValue
 	public DefaultPrefixCache(boolean cache, @Nonnull SQL data, @Nonnull String table, @Nonnull String keyColumn, @Nonnull String valueColumn, @Nonnull String defaultPrefix) {
 		super(data, table, keyColumn, valueColumn, defaultPrefix);
 		this.cacheValues = cache;
 	}
 
+	@CheckReturnValue
 	public DefaultPrefixCache(boolean cache, @Nonnull String defaultPrefix, @Nonnull SQL data, @Nonnull String table, @Nonnull String keyColumn, @Nonnull String valueColumn, int clearRate) {
 		super(cache, defaultPrefix, data, table, keyColumn, valueColumn, clearRate);
 	}
