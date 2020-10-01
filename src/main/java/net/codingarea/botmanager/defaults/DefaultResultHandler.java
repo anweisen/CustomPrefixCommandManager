40b399d681a6cdb211e4f1eb51e2bbe6842094e8
anweisen <crafter8.0@web.de>
2020-10-01 19:14:58 +0200

    Implementing ResultHandler

--- src/main/java/net/codingarea/botmanager/defaults/DefaultResultHandler.java
+++ src/main/java/net/codingarea/botmanager/defaults/DefaultResultHandler.java
@@ -2,11 +2,13 @@ package net.codingarea.botmanager.defaults;
 
 import net.codingarea.botmanager.commandmanager.CommandHandler;
 import net.codingarea.botmanager.commandmanager.CommandResult;
+import net.codingarea.botmanager.commandmanager.ResultHandler;
 import net.codingarea.botmanager.utils.NumberFormatter;
 import net.codingarea.botmanager.utils.TripleConsumer;
 import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 
+import javax.annotation.Nonnull;
 import java.sql.SQLException;
 
 /**
@@ -14,33 +16,47 @@ import java.sql.SQLException;
  * @author anweisen | https://github.com/anweisen
  * @since 2.1
  */
-public final class DefaultResultHandler implements TripleConsumer<MessageReceivedEvent, CommandResult, Object> {
+public final class DefaultResultHandler implements ResultHandler {
 
 	@Override
-	public void accept(MessageReceivedEvent event, CommandResult result, Object obj) {
+	public void handle(@Nonnull MessageReceivedEvent event, @Nonnull CommandResult result, Object arg) {
 
 		String answer = result.getAnswer();
 		if (answer == null) return;
 
-		if (obj instanceof Throwable) {
-			Throwable thrown = (Throwable) obj;
-			String message = String.valueOf(thrown.getMessage());
-			if (thrown instanceof SQLException) {
-				message = "Database error occurred";
-			}
-			answer = answer.replace("%exception%", thrown.getClass().getSimpleName())
-						   .replace("%message%", message);
-		} else if (obj instanceof Permission) {
-			Permission permission = (Permission) obj;
-			answer = answer.replace("%permission%", permission.getName());
-		} else if (obj instanceof Long) {
-			long cooldown = (Long) obj;
-			float seconds = cooldown / 1000F;
-			String formatted = NumberFormatter.DEFAULT.format(seconds);
-			answer = answer.replace("%cooldown%", formatted);
-		} else {
-			String command = String.valueOf(obj);
-			answer = answer.replace("%command%", command).replace("%permission%", "TeamRank");
+		switch (result) {
+			case WEBHOOK_MESSAGE_NO_REACT:
+			case MENTION_PREFIX_NO_REACT:
+			case BOT_MESSAGE_NO_REACT:
+			case COMMAND_NOT_FOUND:
+			case SUCCESS:
+			case PREFIX_NOT_USED:
+			default:
+				break;
+
+			case MEMBER_ON_COOLDOWN:
+				if (arg instanceof Number) {
+					float time = ((Number) arg).floatValue() / 1000F;
+					answer = answer.replace("%cooldown%", NumberFormatter.TIME.format(time));
+				}
+				break;
+			case NO_PERMISSIONS:
+				if (arg instanceof Permission && !event.getMember().hasPermission((Permission) arg)) {
+					answer = answer.replace("%permission%", ((Permission) arg).getName());
+				} else {
+					answer = answer.replace("%permission%", "TeamRank");
+				}
+				break;
+			case EXCEPTION:
+				Throwable ex = null;
+				if (arg instanceof Throwable) {
+					ex = (Throwable) arg;
+					while (ex.getCause() != null) {
+						ex = ex.getCause();
+					}
+				}
+				answer = answer.replace("%exception%", ex != null ? ex.getClass().getSimpleName() : "null");
+				break;
 		}
 
 		event.getChannel().sendMessage(answer).queue();
