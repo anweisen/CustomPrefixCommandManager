c06d612b5ac0cc937faec9d75c2ade2f58ade860
anweisen <crafter8.0@web.de>
2020-10-03 22:16:17 +0200

    Fixed typo ^^

--- src/main/java/net/codingarea/botmanager/commandmanager/CommandResult.java
+++ src/main/java/net/codingarea/botmanager/commandmanager/CommandResult.java
@@ -16,7 +16,7 @@ public enum CommandResult {
 	INVALID_CHANNEL_PRIVATE_COMMAND("You can only use this command in a private chat."),
 	INVALID_CHANNEL_GUILD_COMMAND("You can only use this command in a guild."),
 	NO_PERMISSIONS("You need `%permission%` for that!"),
-	MEMBER_ON_COOLDOWN("You are currently on cooldown for `%cooldown%s`."),
+	MEMBER_ON_COOLDOWN("You are currently on cooldown for `%cooldown%`."),
 	EXCEPTION("Something went wrong: `%exception%`"),
 	WEBHOOK_MESSAGE_NO_REACT,
 	BOT_MESSAGE_NO_REACT,
