f891b0fc2009c1e19ff8b082e20a3d5cfdc551e0
anweisen <crafter8.0@web.de>
2020-10-08 21:25:02 +0200

    Added Update command triggering

--- src/main/java/net/codingarea/engine/discord/commandmanager/CommandHandler.java
+++ src/main/java/net/codingarea/engine/discord/commandmanager/CommandHandler.java
@@ -1,13 +1,19 @@
 package net.codingarea.engine.discord.commandmanager;
 
+import net.codingarea.engine.discord.commandmanager.events.GenericCommandEvent;
 import net.codingarea.engine.utils.LogHelper;
-import net.codingarea.engine.discord.defaults.DefaultPermissionChecker;
+import net.codingarea.engine.discord.defaults.DefaultCommandAccessChecker;
 import net.codingarea.engine.discord.defaults.DefaultResultHandler;
 import net.codingarea.engine.exceptions.CommandExecutionException;
 import net.codingarea.engine.utils.Bindable;
 import net.codingarea.engine.utils.CoolDownManager;
 import net.codingarea.engine.utils.LogLevel;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.events.Event;
+import net.dv8tion.jda.api.events.message.GenericMessageEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.events.message.MessageUpdateEvent;
 
 import javax.annotation.CheckReturnValue;
 import javax.annotation.Nonnull;
@@ -26,7 +32,7 @@ public class CommandHandler implements Bindable {
 	protected ReactionBehavior reactToWebhooks = ReactionBehavior.COMMAND_DECISION;
 	protected ReactionBehavior reactToBots = ReactionBehavior.COMMAND_DECISION;
 	protected CoolDownManager cooldownManager;
-	protected PermissionChecker permissionChecker = new DefaultPermissionChecker();
+	protected CommandAccessChecker accessChecker = new DefaultCommandAccessChecker();
 	protected ResultHandler resultHandler = new DefaultResultHandler();
 
 	@Nonnull
@@ -113,18 +119,44 @@ public class CommandHandler implements Bindable {
 	 * - {@link CommandResult#PREFIX_NOT_USED} if the given prefix and mention prefix was not used <br>
 	 * - {@link CommandResult#MENTION_PREFIX_NO_REACT} the mention prefix was used, but the command should not react <br>
 	 * - {@link CommandResult#COMMAND_NOT_FOUND} if there was not command with the given name <br>
-	 * - {@link CommandResult#MEMBER_ON_COOLDOWN} if the member is on cooldown ({@link #setCoolDownManager(CoolDownManager)}) <br>
+	 * - {@link CommandResult#MEMBER_ON_COOLDOWN} if the member is on cooldown, see {@link #setCoolDownManager(CoolDownManager)} <br>
 	 * - {@link CommandResult#SUCCESS} if the command was executed <br>
 	 * @param prefix the prefix which should be in front of the command
 	 * @param event the {@link MessageReceivedEvent} the command was received
 	 * @see CommandResult
 	 */
 	public void handleCommand(@Nonnull String prefix, @Nonnull MessageReceivedEvent event) {
+		handleCommand(prefix, event, event.getMember(), event.getMessage());
+	}
+
+	/**
+	 * - {@link CommandResult#INVALID_CHANNEL_PRIVATE_COMMAND} if the command was a private command and was performed in a guild chat <br>
+	 * - {@link CommandResult#INVALID_CHANNEL_GUILD_COMMAND} if the command was a guild command and was performed in a private chat <br>
+	 * - {@link CommandResult#WEBHOOK_MESSAGE_NO_REACT} if the message came from a webhook, and the command should not react <br>
+	 * - {@link CommandResult#BOT_MESSAGE_NO_REACT} if the message came from a bot, and the command should not react <br>
+	 * - {@link CommandResult#PREFIX_NOT_USED} if the given prefix and mention prefix was not used <br>
+	 * - {@link CommandResult#MENTION_PREFIX_NO_REACT} the mention prefix was used, but the command should not react <br>
+	 * - {@link CommandResult#COMMAND_NOT_FOUND} if there was not command with the given name <br>
+	 * - {@link CommandResult#MEMBER_ON_COOLDOWN} if the member is on cooldown, see {@link #setCoolDownManager(CoolDownManager)} <br>
+	 * - {@link CommandResult#SUCCESS} if the command was executed <br>
+	 * @param prefix the prefix which should be in front of the command
+	 * @param event the {@link MessageReceivedEvent} the command was received
+	 * @see CommandResult
+	 */
+	public void handleCommand(@Nonnull String prefix, @Nonnull MessageUpdateEvent event) {
+		handleCommand(prefix, event, event.getMember(), event.getMessage());
+	}
+
+	/**
+	 * @see #handleCommand(String, MessageUpdateEvent)
+	 * @see #handleCommand(String, MessageReceivedEvent)
+	 */
+	protected void handleCommand(@Nonnull String prefix, @Nonnull GenericMessageEvent event, Member member, @Nonnull Message message) {
 
 		ResultHandler resultHandler = this.resultHandler;
-		if (resultHandler == null) resultHandler = (e, r, a) -> {};
+		if (resultHandler == null) resultHandler = (a, b, c, d, e, f) -> {};
 
-		String raw = event.getMessage().getContentRaw().toLowerCase().trim();
+		String raw = message.getContentRaw().toLowerCase().trim();
 		boolean byMention = false;
 		String mention = mention(event);
 
@@ -155,8 +187,8 @@ public class CommandHandler implements Bindable {
 			return;
 		}
 
-		if (cooldownManager != null && cooldownManager.isOnCoolDown(event.getMember())) {
-			resultHandler.handle(event, CommandResult.MEMBER_ON_COOLDOWN, cooldownManager.getCoolDownLeft(event.getMember()));
+		if (cooldownManager != null && cooldownManager.isOnCoolDown(member)) {
+			resultHandler.handle(event, CommandResult.MEMBER_ON_COOLDOWN, cooldownManager.getCoolDownLeft(member));
 			return;
 		} else if (command.getType() == CommandType.GUILD && !event.isFromGuild()) {
 			resultHandler.handle(event, CommandResult.INVALID_CHANNEL_GUILD_COMMAND, commandName.trim());
@@ -167,24 +199,28 @@ public class CommandHandler implements Bindable {
 		} else if (!command.shouldReactToMentionPrefix() && byMention) {
 			resultHandler.handle(event, CommandResult.MENTION_PREFIX_NO_REACT, null);
 			return;
-		} else if (event.getMember() != null && permissionChecker != null && !permissionChecker.isAllowed(event.getMember(), command)) {
+		} else if (!command.executeOnUpdate() && event instanceof MessageUpdateEvent) {
+			resultHandler.handle(event, CommandResult.MESSAGE_EDIT_NO_REACT, null);
+			return;
+		} else if (member != null && accessChecker != null && !accessChecker.isAllowed(member, command)) {
 			resultHandler.handle(event, CommandResult.NO_PERMISSIONS, command.getPermissionNeeded());
 			return;
-		} else if (reactToWebhooks != ReactionBehavior.ALWAYS && event.isWebhookMessage() && (!command.shouldReactToWebhooks() || reactToWebhooks == ReactionBehavior.NEVER)) {
+		} else if (reactToWebhooks != ReactionBehavior.ALWAYS && message.isWebhookMessage() && (!command.shouldReactToWebhooks() || reactToWebhooks == ReactionBehavior.NEVER)) {
 			resultHandler.handle(event, CommandResult.WEBHOOK_MESSAGE_NO_REACT, null);
 			return;
-		} else if (reactToBots != ReactionBehavior.ALWAYS && event.getAuthor().isBot() && (!command.shouldReactToBots() || reactToBots == ReactionBehavior.NEVER)) {
+		} else if (reactToBots != ReactionBehavior.ALWAYS && message.getAuthor().isBot() && (!command.shouldReactToBots() || reactToBots == ReactionBehavior.NEVER)) {
 			resultHandler.handle(event, CommandResult.BOT_MESSAGE_NO_REACT, null);
 			return;
 		}
 
-		if (cooldownManager != null) cooldownManager.addToCoolDown(event.getMember());
+		if (cooldownManager != null)
+			cooldownManager.addToCoolDown(member);
 
-		process(command, new CommandEvent(prefix, getCommandName(command, commandName), event), resultHandler);
+		process(command, GenericCommandEvent.ofEvent(event, prefix, getCommandName(command, commandName)), resultHandler);
 
 	}
 
-	protected String mention(MessageReceivedEvent event) {
+	protected String mention(Event event) {
 		return "<@!" + event.getJDA().getSelfUser().getId() + ">";
 	}
 
@@ -206,14 +242,17 @@ public class CommandHandler implements Bindable {
 		return this;
 	}
 
+	@CheckReturnValue
 	public ReactionBehavior getBotMessageBehavior() {
 		return reactToBots;
 	}
 
+	@CheckReturnValue
 	public ReactionBehavior getWebhookMessageBehavior() {
 		return reactToWebhooks;
 	}
 
+	@CheckReturnValue
 	public CoolDownManager getCoolDownManager() {
 		return cooldownManager;
 	}
@@ -231,13 +270,13 @@ public class CommandHandler implements Bindable {
 	 * @return <code>this</code> for chaining
 	 */
 	@Nonnull
-	public CommandHandler setPermissionChecker(PermissionChecker permissionChecker) {
-		this.permissionChecker = permissionChecker;
+	public CommandHandler setPermissionChecker(CommandAccessChecker accessChecker) {
+		this.accessChecker = accessChecker;
 		return this;
 	}
 
-	public PermissionChecker getPermissionChecker() {
-		return permissionChecker;
+	public CommandAccessChecker getPermissionChecker() {
+		return accessChecker;
 	}
 
 	/**
@@ -249,6 +288,7 @@ public class CommandHandler implements Bindable {
 		return this;
 	}
 
+	@CheckReturnValue
 	public ResultHandler getResultHandler() {
 		return resultHandler;
 	}
@@ -270,9 +310,9 @@ public class CommandHandler implements Bindable {
 	private static void execute(@Nonnull ICommand command, @Nonnull CommandEvent event, ResultHandler result) throws CommandExecutionException {
 		try {
 			command.onCommand(event);
-			result.handle(event.getReceivedEvent(), CommandResult.SUCCESS, null);
+			result.handle(event.getEvent(), CommandResult.SUCCESS, null);
 		} catch (Throwable ex) {
-			result.handle(event.getReceivedEvent(), CommandResult.EXCEPTION, ex);
+			result.handle(event.getEvent(), CommandResult.EXCEPTION, ex);
 			throw new CommandExecutionException(ex);
 		}
 	}
