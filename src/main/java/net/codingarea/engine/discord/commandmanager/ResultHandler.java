5339b2c979bcc1de44a04bb31c799f8ce13129d9
anweisen <crafter8.0@web.de>
2020-10-08 21:25:38 +0200

    Updated ResltHandler for different MessageEvents

--- src/main/java/net/codingarea/engine/discord/commandmanager/ResultHandler.java
+++ src/main/java/net/codingarea/engine/discord/commandmanager/ResultHandler.java
@@ -1,8 +1,14 @@
 package net.codingarea.engine.discord.commandmanager;
 
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.MessageChannel;
+import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.events.message.GenericMessageEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.events.message.MessageUpdateEvent;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 
 /**
  * @author anweisen | https://github.com/anweisen
@@ -10,6 +16,18 @@ import javax.annotation.Nonnull;
  */
 public interface ResultHandler {
 
-	void handle(@Nonnull MessageReceivedEvent event, @Nonnull CommandResult result, Object arg);
+	void handle(@Nullable GenericMessageEvent event, @Nonnull MessageChannel channel, @Nonnull User user, @Nullable Member member, @Nonnull CommandResult result, Object arg);
+
+	default void handle(@Nonnull GenericMessageEvent event, @Nonnull CommandResult result, Object arg) {
+		if (event instanceof MessageReceivedEvent) {
+			MessageReceivedEvent receivedEvent = (MessageReceivedEvent) event;
+			handle(event, event.getChannel(), receivedEvent.getAuthor(), receivedEvent.getMember(), result, arg);
+		} else if (event instanceof MessageUpdateEvent) {
+			MessageUpdateEvent updatedEvent = (MessageUpdateEvent) event;
+			handle(event, event.getChannel(), updatedEvent.getAuthor(), updatedEvent.getMember(), result, arg);
+		} else {
+			throw new IllegalArgumentException(event.getClass().getName() + " is not supported!");
+		}
+	}
 
 }
