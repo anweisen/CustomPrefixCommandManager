8c3618bc89470cd77aa1b027a601f53748768fea
anweisen <crafter8.0@web.de>
2020-10-11 23:15:04 +0200

    Improved StringBuilderWriter

--- src/main/java/net/codingarea/engine/utils/StringBuilderWriter.java
+++ src/main/java/net/codingarea/engine/utils/StringBuilderWriter.java
@@ -12,20 +12,56 @@ import java.io.Writer;
  */
 public class StringBuilderWriter extends Writer {
 
-	protected final StringBuilder builder = new StringBuilder();
+	protected final StringBuilder builder;
 
-	@Override
-	public void write(@NotNull char[] cbuf, int off, int len) {
-		builder.append(cbuf, off, len);
+	public StringBuilderWriter() {
+		builder = new StringBuilder();
+	}
+
+	public StringBuilderWriter(int capacity) {
+		builder = new StringBuilder(capacity);
+	}
+
+	public StringBuilderWriter(StringBuilder builder) {
+		this.builder = builder != null ? builder : new StringBuilder();
+	}
+
+	public Writer append(char value) {
+		builder.append(value);
+		return this;
+	}
+
+	public Writer append(CharSequence value) {
+		builder.append(value);
+		return this;
+	}
+
+	public Writer append(CharSequence value, int start, int end) {
+		builder.append(value, start, end);
+		return this;
+	}
+
+	public void close() {
+	}
+
+	public void flush() {
+	}
+
+	public void write(@NotNull String value) {
+		builder.append(value);
+	}
+
+	public void write(char[] value, int offset, int length) {
+		if (value != null) {
+			builder.append(value, offset, length);
+		}
 	}
 
 	public StringBuilder getBuilder() {
 		return builder;
 	}
 
-	@Override
-	public void flush() { }
-
-	@Override
-	public void close() { }
+	public String toString() {
+		return builder.toString();
+	}
 }
