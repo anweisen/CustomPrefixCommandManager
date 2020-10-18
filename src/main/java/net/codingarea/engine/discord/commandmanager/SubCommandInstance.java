b5248d7386609c469c602f031692d42be20ef850
anweisen <crafter8.0@web.de>
2020-10-18 18:33:00 +0200

    Added command descriptions & Updated SubCommandNames

--- src/main/java/net/codingarea/engine/discord/commandmanager/SubCommandInstance.java
+++ src/main/java/net/codingarea/engine/discord/commandmanager/SubCommandInstance.java
@@ -5,6 +5,7 @@ import net.codingarea.engine.exceptions.IllegalSubCommandNameException;
 
 import javax.annotation.Nonnull;
 import java.lang.reflect.Method;
+import java.util.Arrays;
 
 /**
  * @author anweisen | https://github.com/anweisen
@@ -12,8 +13,7 @@ import java.lang.reflect.Method;
  */
 public final class SubCommandInstance {
 
-	private final String name;
-	private final String[] alias;
+	private final String[] names;
 	private final Class<?>[] args;
 	private final Method method;
 	private final SubCommandHandler root;
@@ -23,35 +23,37 @@ public final class SubCommandInstance {
 		this.root = root;
 
 		if (!method.isAnnotationPresent(SubCommand.class))
-			throw new IllegalSubCommandException(method);
+			throw new IllegalSubCommandException("Method is not annotated as net.codingarea.engine.commandmanager.SubCommand", method);
 
 		if (method.getReturnType() != void.class)
-			throw new IllegalSubCommandException(method);
+			throw new IllegalSubCommandException("Method does not return void", method);
 
 		Class<?>[] args = method.getParameterTypes();
 
 		if (args.length < 1)
-			throw new IllegalSubCommandException(method);
+			throw new IllegalSubCommandException("Method has 0 arguments", method);
 
 		if (args[0] != CommandEvent.class)
-			throw new IllegalSubCommandException(method);
+			throw new IllegalSubCommandException("First argument is not net.codingarea.engine.commandmanager.CommandEvent", method);
 
 		this.args = new Class[args.length - 1];
 		System.arraycopy(args, 1, this.args, 0, args.length - 1);
 
 		SubCommand command = method.getAnnotation(SubCommand.class);
+		String[] names = command.name();
 
-		this.name = command.name();
-		this.alias = command.alias();
-		this.method = method;
-
-		if (name.contains(" "))
-			throw new IllegalSubCommandNameException(name);
-
-		for (String alias : this.alias) {
-			if (alias == null || alias.contains(" "))
-				throw new IllegalSubCommandNameException(alias);
+		if (names.length == 0) {
+			names = new String[] {method.getName()};
+		} else {
+			// Making sure no name contains a space
+			for (String alias : names) {
+				if (alias == null || alias.contains(" "))
+					throw new IllegalSubCommandNameException(alias);
+			}
 		}
+
+		this.names = names;
+		this.method = method;
 
 	}
 
@@ -69,16 +71,16 @@ public final class SubCommandInstance {
 		return method;
 	}
 
-	public String getName() {
-		return name;
+	public String[] getNames() {
+		return Arrays.copyOf(names, names.length);
 	}
 
-	public String[] getAlias() {
-		return alias;
+	public String getName() {
+		return names[0];
 	}
 
 	public Class<?>[] getArgs() {
-		return args;
+		return Arrays.copyOf(args, args.length);
 	}
 
 	public SubCommandHandler getRoot() {
