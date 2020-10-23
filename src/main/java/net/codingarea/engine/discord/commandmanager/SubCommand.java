9cb30767d43fc26182934101cfdb6d96c4ee18de
anweisen <crafter8.0@web.de>
2020-10-24 01:38:19 +0200

    Added comments

--- src/main/java/net/codingarea/engine/discord/commandmanager/SubCommand.java
+++ src/main/java/net/codingarea/engine/discord/commandmanager/SubCommand.java
@@ -4,6 +4,7 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import java.lang.reflect.Method;
 
 /**
  * @author anweisen | https://github.com/anweisen
@@ -13,6 +14,12 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 public @interface SubCommand {
 
+	/**
+	 * Represents the name and aliases for the {@link SubCommand SubCommand} <br>
+	 * If the value is left at default value, the {@link Method#getName() MethodName} is used
+	 *
+	 * @return The names for the {@link SubCommand SubCommand}
+	 */
 	String[] name() default {};
 
 }
