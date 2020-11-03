27c144a69321dc7a52c0fc020dfe1c30eb51e209
anweisen <anweisen@web.de>
2020-11-03 16:27:30 +0100

    Created HelpMessageBuilder

--- src/main/java/net/codingarea/engine/utils/Embeds.java
+++ src/main/java/net/codingarea/engine/utils/Embeds.java
@@ -1,12 +1,22 @@
 package net.codingarea.engine.utils;
 
+import net.codingarea.engine.discord.commandmanager.CommandEvent;
+import net.codingarea.engine.discord.commandmanager.ICommand;
 import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.MessageEmbed;
+import net.dv8tion.jda.api.entities.MessageEmbed.Field;
 import net.dv8tion.jda.api.entities.User;
 
 import javax.annotation.CheckReturnValue;
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author anweisen | https://github.com/anweisen
@@ -103,6 +113,168 @@ public final class Embeds {
 	@CheckReturnValue
 	public static String title(@Nonnull User user, String suffix) {
 		return title(user.getEffectiveAvatarUrl(), user.getName(), suffix);
+	}
+
+	public static class HelpMessageBuilder {
+
+		public static class EmbedField {
+
+			private final String title;
+			private final List<String> lines = new ArrayList<>();
+
+			public EmbedField(final @Nonnull String title) {
+				this.title = title;
+			}
+
+			@Nonnull
+			public EmbedField add(final @Nonnull String content) {
+				lines.add(content);
+				return this;
+			}
+
+			@Nonnull
+			public EmbedField add(final @Nonnull String command, final @Nullable String description) {
+				return add("» " + command + (description != null ? " • " + description : ""));
+			}
+
+			@Nonnull
+			public EmbedField add(final @Nonnull String command, final @Nonnull String description,
+			                      final @Nonnull CommandEvent event, final @Nonnull Permission... permission) {
+				return event.senderHasPermission(permission) ? add(command, description) : this;
+			}
+
+			@Nonnull
+			public EmbedField add(final @Nonnull CommandEvent event, final @Nonnull Class<? extends ICommand> commandClass) {
+				ICommand command = event.getHandler().getCommand(commandClass);
+				if (command == null)
+					throw new IllegalStateException("No command of the class " + commandClass + " is registered");
+				return add(event, command);
+			}
+
+			@Nonnull
+			public EmbedField add(final @Nonnull CommandEvent event, final @Nonnull ICommand command) {
+				return event.getHandler().hasAccess(event.getMember(), command) ?
+						add(CommandEvent.syntax(event, command.getName(), false), command.getDescription()) : this;
+			}
+
+			@Nonnull
+			@SafeVarargs
+			public final EmbedField add(final @Nonnull CommandEvent event, final @Nonnull Class<? extends ICommand>... commandClasses) {
+				for (Class<? extends ICommand> commandClass : commandClasses) {
+					add(event, commandClass);
+				}
+				return this;
+			}
+
+			@Nonnull
+			@CheckReturnValue
+			public Field toEmbedField() {
+				StringBuilder content = new StringBuilder();
+				for (String line : lines) {
+					content.append("\n" + line);
+				}
+				return new Field(title + " (" + lines.size() + ")", content.toString(), false);
+			}
+
+		}
+
+		private final String avatar;
+		private final String botName;
+		private final List<EmbedField> fields = new ArrayList<>();
+
+		private String authorURL;
+		private Color color;
+		private CharSequence description;
+
+		public HelpMessageBuilder(final @Nonnull CommandEvent event) {
+			this(event.getJDA());
+			this.color = event.getSelfColorNotNull();
+		}
+
+		public HelpMessageBuilder(final @Nonnull CommandEvent event, final @Nonnull Permission... permissions) {
+			this(event);
+			setAuthorURL(event.getJDA(), permissions);
+		}
+
+		public HelpMessageBuilder(final @Nonnull JDA jda) {
+			avatar = jda.getSelfUser().getEffectiveAvatarUrl();
+			botName = jda.getSelfUser().getName();
+		}
+
+		public HelpMessageBuilder(final @Nonnull Member selfMember, final @Nonnull Permission... permissions) {
+			this(selfMember.getJDA());
+			setAuthorURL(selfMember.getJDA(), permissions);
+		}
+
+		@Nonnull
+		public HelpMessageBuilder setColor(final @Nullable Color color) {
+			this.color = color;
+			return this;
+		}
+
+		@Nonnull
+		public HelpMessageBuilder setColor(final @Nullable String hex) {
+			this.color = hex == null ? null : Color.decode(hex.startsWith("#") ? hex : "#" + hex);
+			return this;
+		}
+
+		@Nonnull
+		HelpMessageBuilder setAuthorURL(final @Nullable String url) {
+			this.authorURL = url;
+			return this;
+		}
+
+		@Nonnull
+		HelpMessageBuilder setAuthorURL(final @Nonnull JDA jda, final @Nonnull Permission... permissions) {
+			this.authorURL = jda.getInviteUrl(permissions);
+			return this;
+		}
+
+		@Nonnull
+		public HelpMessageBuilder setDescription(final @Nonnull CharSequence description) {
+			this.description = description;
+			return this;
+		}
+
+		@Nonnull
+		public HelpMessageBuilder addField(final @Nonnull EmbedField field) {
+			fields.add(field);
+			return this;
+		}
+
+		@Nonnull
+		public HelpMessageBuilder addField(final @Nonnull EmbedField field, boolean add) {
+			if (add) fields.add(field);
+			return this;
+		}
+
+
+		@Nonnull
+		@CheckReturnValue
+		public EmbedBuilder embed() {
+
+			EmbedBuilder embed = new EmbedBuilder();
+
+			embed.setColor(color);
+			embed.setDescription(description);
+			embed.setAuthor(title(avatar, botName, "Information"), authorURL, avatar);
+
+			for (EmbedField field : fields) {
+				Field embedField = field.toEmbedField();
+				if (embedField.getValue() != null && embedField.getValue().trim().length() > 1)
+					embed.addField(embedField);
+			}
+
+			return embed;
+
+		}
+
+		@Nonnull
+		@CheckReturnValue
+		public MessageEmbed build() {
+			return embed().build();
+		}
+
 	}
 
 }
