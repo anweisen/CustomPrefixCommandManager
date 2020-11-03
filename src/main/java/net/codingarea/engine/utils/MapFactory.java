5addd271762586e0b7a802c1fdf23c3884d629e3
anweisen <anweisen@web.de>
2020-11-03 16:37:46 +0100

    Created MapFactiry#create

--- src/main/java/net/codingarea/engine/utils/MapFactory.java
+++ src/main/java/net/codingarea/engine/utils/MapFactory.java
@@ -67,4 +67,16 @@ public final class MapFactory {
 		return sorted;
 	}
 
+	@Nonnull
+	@CheckReturnValue
+	public static <K, V> Map<K, V> create(K[] keys, V[] values) {
+		if (keys.length != values.length)
+			throw new IllegalArgumentException();
+		Map<K, V> map = new HashMap<>();
+		for (int i = 0; i < keys.length; i++) {
+			map.put(keys[i], values[i]);
+		}
+		return map;
+	}
+
 }
