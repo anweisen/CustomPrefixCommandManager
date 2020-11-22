45fb8f44c3ec26b854dbc9ed518c80d15ed49dfc
anweisen <anweisen@web.de>
2020-11-22 22:28:47 +0100

    Create ThrowingPredicate.java

---
+++ src/main/java/net/codingarea/engine/utils/function/ThrowingPredicate.java
@@ -0,0 +1,24 @@
+package net.codingarea.engine.utils.function;
+
+import net.codingarea.engine.exceptions.ExecutionException;
+
+import java.util.function.Predicate;
+
+/**
+ * @author anweisen | https://github.com/anweisen
+ * @since 2.8
+ */
+public interface ThrowingPredicate<T> extends Predicate<T> {
+
+	@Override
+	default boolean test(T t) {
+		try {
+			return testExceptionally(t);
+		} catch (Exception ignored) {
+			throw new ExecutionException();
+		}
+	}
+
+	boolean testExceptionally(T t) throws Exception;
+
+}
