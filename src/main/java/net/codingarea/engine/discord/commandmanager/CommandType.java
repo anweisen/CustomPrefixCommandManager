038546fc52dad9e411ee3bb28de1f62d6915cf0c
anweisen <anweisen@web.de>
2020-12-03 22:02:28 +0100

    Improved CommandTypes

--- src/main/java/net/codingarea/engine/discord/commandmanager/CommandType.java
+++ src/main/java/net/codingarea/engine/discord/commandmanager/CommandType.java
@@ -1,32 +1,43 @@
 package net.codingarea.engine.discord.commandmanager;
 
-import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
-
 /**
- * @see ICommand
- * @see CommandHandler
  * @author anweisen | https://github.com/anweisen
  * @since 1.2
+ *
+ * @see ICommand
+ * @see ICommand#getType()
+ * @see ICommandHandler
  */
 public enum CommandType {
 
 	/**
-	 * Commands of this type are {@link CommandType#PRIVATE} and {@link CommandType#GUILD}
+	 * Commands of this type are {@link CommandType#PRIVATE} and {@link CommandType#GUILD}.
+	 * They can be used in both cases
 	 */
 	GENERAL,
 
 	/**
-	 * Commands of this type can only be executed in a private chat
-	 * If this is not the case {@link CommandHandler#handleCommand(String, MessageReceivedEvent)}
-	 * will return {@link CommandResult#INVALID_CHANNEL_PRIVATE_COMMAND}
+	 * Commands of this type can only be executed in a private chat.
+	 * If this is not the case {@link CommandResult#INVALID_CHANNEL_GUILD_COMMAND} will be returned
+	 *
+	 * @see #isAccessibleFromPrivate()
 	 */
 	PRIVATE,
 
 	/**
-	 * Commands of this type can only be executed in a guild chat
-	 * If this is not the case {@link CommandHandler#handleCommand(String, MessageReceivedEvent)}
-	 * will return {@link CommandResult#INVALID_CHANNEL_GUILD_COMMAND}
+	 * Commands of this type can only be executed in a guild chat.
+	 * If this is not the case {@link CommandResult#INVALID_CHANNEL_GUILD_COMMAND} will be returned
+	 *
+	 * @see #isAccessibleFromGuild()
 	 */
 	GUILD;
+
+	public boolean isAccessibleFromGuild() {
+		return this == GENERAL || this == GUILD;
+	}
+
+	public boolean isAccessibleFromPrivate() {
+		return this == GENERAL || this == PRIVATE;
+	}
 
 }
