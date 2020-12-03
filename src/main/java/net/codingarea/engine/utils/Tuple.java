118fa09c7806784251d386c3f0d2fc040c797bc9
anweisen <anweisen@web.de>
2020-12-03 22:01:41 +0100

    Created ofA & ofB utils

--- src/main/java/net/codingarea/engine/utils/Tuple.java
+++ src/main/java/net/codingarea/engine/utils/Tuple.java
@@ -1,32 +1,44 @@
 package net.codingarea.engine.utils;
 
+import javax.annotation.Nullable;
+
 /**
  * @author anweisen | https://github.com/anweisen
  * @since 2.6
  */
 public class Tuple<A, B> {
 
+	public static <A, B> Tuple<A, B> ofA(@Nullable A a) {
+		return new Tuple<>(a, null);
+	}
+
+	public static <A, B> Tuple<A, B> ofB(@Nullable B b) {
+		return new Tuple<>(null, b);
+	}
+
 	protected A a;
 	protected B b;
 
-	public Tuple(A a, B b) {
+	public Tuple(@Nullable A a, @Nullable B b) {
 		this.a = a;
 		this.b = b;
 	}
 
+	@Nullable
 	public A a() {
 		return a;
 	}
 
+	@Nullable
 	public B b() {
 		return b;
 	}
 
-	public void a(A a) {
+	public void a(@Nullable A a) {
 		this.a = a;
 	}
 
-	public void b(B b) {
+	public void b(@Nullable B b) {
 		this.b = b;
 	}
 
