cc55b96cac71ede98259d1bbd8b5f9c34ff783f7
anweisen <anweisen@web.de>
2020-12-15 21:42:04 +0100

    Added comparision method

--- src/main/java/net/codingarea/engine/utils/INamed.java
+++ src/main/java/net/codingarea/engine/utils/INamed.java
@@ -14,4 +14,19 @@ public interface INamed {
 	@CheckReturnValue
 	String getName();
 
+	@CheckReturnValue
+	static boolean matches(@Nonnull Object object, @Nonnull String name) {
+		if (object instanceof INamed) {
+			if (((INamed) object).getName().equalsIgnoreCase(name))
+				return true;
+		}
+		if (object instanceof IAlias) {
+			for (String alias : ((IAlias) object).getAlias()) {
+				if (alias.equalsIgnoreCase(name))
+					return true;
+			}
+		}
+		return false;
+	}
+
 }
